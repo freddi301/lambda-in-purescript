ba874a342f190dad19e0a9d94f036165ac0dcb74
Frederik Batuna <gobi301@gmail.com>
2017-11-07 13:00:58 +0100

    refactor: better helper ast constructors

--- src/Main.purs
+++ src/Main.purs
@@ -9,14 +9,25 @@ import Data.Maybe (fromMaybe)
 
 data Ast = Reference String | Application Ast Ast | Abstraction String Ast
 
-infixr 5 Abstraction as \
-infixl 4 Application as !
-r :: String -> Ast
-r = Reference
+-- operators for friendlier construction of the ast
+-- λx.x x
+-- "x" \ "x" ! "x"
+class ToAstAbs body where toAstAbs :: String -> body -> Ast
+instance toAstAbsString :: ToAstAbs String where toAstAbs head body = Abstraction head (Reference body) 
+instance toAstAbsAst :: ToAstAbs Ast where toAstAbs = Abstraction
+class ToAstApp left right where toAstApp :: left -> right -> Ast
+instance toAstAppStringString :: ToAstApp String String where toAstApp left right = Application (Reference left) (Reference right)
+instance toAstAppStringAst :: ToAstApp String Ast where toAstApp left right = Application (Reference left) right
+instance toAstAppAstString :: ToAstApp Ast String where toAstApp left right = Application left (Reference right)
+instance toAstAppAstAst :: ToAstApp Ast Ast where toAstApp left right = Application left right
+infixr 8 toAstAbs as \
+infixl 9 toAstApp as !
 
 instance showAst :: Show Ast where
   show (Reference name) = name
+  show (Application (Application leftLeft leftRight) right) = "(" <> show leftLeft <> " " <> show leftRight <> " " <> show right <> ")"
   show (Application left right) = "(" <> show left <> " " <> show right <> ")"
+  show (Abstraction head (Abstraction headRight bodyRight)) = "(" <> head <> " => " <> headRight <> " => " <> show bodyRight <> ")"
   show (Abstraction head body) = "(" <> head <> " => " <> show body <> ")"
 
 derive instance eqAst :: Eq Ast
--- test/Main.purs
+++ test/Main.purs
@@ -4,14 +4,17 @@ import Main
 
 import Control.Monad.Eff (Eff)
 import Data.Map (empty)
-import Prelude (Unit, discard, show, (==), ($))
+import Prelude (Unit, discard, show, (==))
 import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 
 eval :: Ast -> { scope :: Scope Ast, term :: Ast }
-eval term = evaluate { scope: empty, term } 
+eval term = evaluate { scope: empty, term }
+
+ev :: Ast -> Ast
+ev term = (evaluate { scope: empty, term }).term
 
 main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] do
@@ -20,11 +23,34 @@ main = run [consoleReporter] do
       let action = show (Abstraction "a" (Application (Reference "a") (Reference "a")))
       let result = "(a => (a a))"
       action `shouldEqual` result
+      shouldEqual (show ("x" \ "y" \ "x")) "(x => y => x)"
+      shouldEqual (show ("x" \ ("x" ! "x" ! "x"))) "(x => (x x x))"
   describe "eq Ast" do
     it "works" do
       ((Reference "a") == (Reference "a")) `shouldEqual` true
       ((Reference "a") == (Reference "b")) `shouldEqual` false
+      let true_ = "x" \ "y" \ "x"
+      let false_ = "x" \ "y" \ "y"
+      (true_ == false_) `shouldEqual` false
   describe "evaluate" do
-    it "evaluates identity" do
-      (eval $ "x" \ r"x" ! "y" \ r"y").term `shouldEqual` ("y" \ r"y")
-    pending "more evaluations"
+    it "identity" do
+      (eval (("x" \ "x") ! ("y" \ "y"))).term `shouldEqual` ("y" \ "y")
+    describe "booleans" do
+      let true_ = "x" \ "y" \ "x"
+      let false_ = "x" \ "y" \ "y"
+      let not_ = "p" \ "p" ! false_ ! true_
+      let and_ = "a" \ "b" \ "a" ! "b" ! false_
+      let or_ = "a" \ "b" \ "a" ! true_ ! "b"
+      it "respects 'not' truth table" do
+        (eval (not_ ! true_)).term `shouldEqual` false_
+        (eval (not_ ! false_)).term `shouldEqual` true_
+      it "respects 'and' truth table" do
+        ev (and_ ! true_ ! true_) `shouldEqual` true_
+        ev (and_ ! true_ ! false_) `shouldEqual` false_
+        ev (and_ ! false_ ! true_) `shouldEqual` false_
+        ev (and_ ! false_ ! false_) `shouldEqual` false_
+      pending "or truth table"
+      pending "nested example"
+    pending "list"
+    pending "church numerals"
+    pending "Y combinator"
