a1ca784656bef8f53965b83ba3d559fdf0fe749a
Frederik Batuna <gobi301@gmail.com>
2018-05-07 14:02:10 +0200

    add: reify eager enhance test

--- test/Lip/Evaluate/Reify/Eager.purs
+++ test/Lip/Evaluate/Reify/Eager.purs
@@ -1,9 +1,12 @@
 module Test.Lip.Evaluate.Reify.Eager where
 
+import Data.Either
 import Lip.Evaluate.Reify.Eager
 import Prelude
+import Prelude
 
-import Data.Either
+import Lambda.Data.Ast (ref)
+import Lip.Data.Ast (Ast(..))
 import Lip.Data.Ast.Helpers ((\), (!))
 import Lip.Evaluate (Evaluate)
 import Prelude (Unit, unit, discard, (>>>))
@@ -15,7 +18,35 @@ import Test.Spec.Runner (RunnerEffects)
 test :: ∀ e . Spec (RunnerEffects e) Unit
 test = describe "reify eager" do
   describe "evaluate" $ BooleanTest.test evaluate
-  describe "enhance" $ BooleanTest.test $ enhance id
+  describe "enhance" do
+    BooleanTest.test $ enhance id
+    describe "booleans" do
+      let evaluatesTo source result = (enhance globals source) `shouldEqual` (enhance globals source)
+      it "respects 'not' truth table" do
+        ("NOT" ! "TRUE") `evaluatesTo` (ref "FALSE")
+        ("NOT" ! "FALSE") `evaluatesTo` (ref "TRUE")
+      it "respects 'and' truth table" do
+        ("AND" ! "TRUE" ! "TRUE") `evaluatesTo` (ref "TRUE")
+        ("AND" ! "TRUE" ! "FALSE") `evaluatesTo` (ref "FALSE")
+        ("AND" ! "FALSE" ! "TRUE") `evaluatesTo` (ref "FALSE")
+        ("AND" ! "FALSE" ! "FALSE") `evaluatesTo` (ref "FALSE")
+      it "respects 'or' truth table" do
+        ("OR" ! "TRUE" ! "TRUE") `evaluatesTo` (ref "TRUE")
+        ("OR" ! "TRUE" ! "FALSE") `evaluatesTo` (ref "TRUE")
+        ("OR" ! "FALSE" ! "TRUE") `evaluatesTo` (ref "TRUE")
+        ("OR" ! "FALSE" ! "FALSE") `evaluatesTo` (ref "FALSE")
+      it "works with nested" do
+        ("OR" ! ("AND" ! "TRUE" ! "TRUE") ! ("NOT" ! "TRUE")) `evaluatesTo` (ref "TRUE")
   describe "stop" do
     BooleanTest.test $ \ast -> (either (const ast) id) (stop Right ast)
     pending "stop on ast node"
+
+globals :: Evaluate String Unit -> Evaluate String Unit
+globals evaluate ast = evaluate replace where
+  replace  = case ast of
+    Reference "TRUE" _ -> "x" \ "y" \ "x"
+    Reference "FALSE" _ -> "x" \ "y" \ "y"
+    Reference "NOT" _ -> "p" \ "p" ! "FALSE" ! "TRUE"
+    Reference "AND" _ -> "a" \ "b" \ "a" ! "b" ! "FALSE"
+    Reference "OR" _ -> "a" \ "b" \ "a" ! "FALSE" ! "b"
+    _ -> ast
